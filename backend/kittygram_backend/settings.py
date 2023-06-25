4deddb0176290294b28cc5ede4adebc0d4758758
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-06-25 11:08:49 +0300

    test

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -90,11 +90,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
