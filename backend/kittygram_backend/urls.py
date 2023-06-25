4a8c4a3f0c5755de7712dbc38f47e9075310338f
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-06-25 23:19:39 +0300

    Add Actions

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/', include('djoser.urls')),  # Работа с пользователями
-    path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
+    path('api/', include('djoser.urls.authtoken')),
 ]
 
 if settings.DEBUG:
