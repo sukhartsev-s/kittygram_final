94f8a6eb0e4d0a65e7b06612eddceb1e8a75b449
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-06-27 09:53:03 +0300

    Add Actions

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -90,10 +90,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
+
 STATIC_ROOT = BASE_DIR / "collected_static"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = "/media"
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
