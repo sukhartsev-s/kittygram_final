63e6a1933dda3f2f112741f415f77356ee3b65ca
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-06-28 10:31:27 +0300

    settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -100,7 +100,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -109,5 +109,4 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-
 }
