d51ebdd5e9e0a302ee23e473f5ac963821d571ab
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-06-30 10:00:46 +0300

    key

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure")
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-123")
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
